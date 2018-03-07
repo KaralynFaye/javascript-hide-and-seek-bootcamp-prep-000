18a7994a5086c7bfd528c3deb713713301dc457e
Karalyn Ferrari <lifesmygift@gmail.com>
2018-03-07 14:36:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ function increaseRankBy(n) {
 
 function deepestChild() {
   var letsSee= document.querySelector('#grand-node');
+  return letsSee.children;
   while (letsSee.children) {
     letsSee=letsSee.children[0];
   }
