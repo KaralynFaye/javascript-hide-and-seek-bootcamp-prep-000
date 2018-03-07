c6be18c9726e6d678b28388cee2d75dda196af58
Karalyn Ferrari <lifesmygift@gmail.com>
2018-03-07 14:23:44 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,19 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested');
+}
+
+function increaseRankBy(n) {
+  const ranks = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (var i= 0; i <ranks.length; i++) {
+    ranks[i].innerHTML = (parseInt(ranks[i].innerHTML) + 3).toString();
+  }
+  
+}
+
+function deepestChild() {
+  var 
+}
